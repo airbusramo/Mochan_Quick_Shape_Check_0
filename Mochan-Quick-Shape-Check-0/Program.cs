dc16ab90368c5ce75441b1cdf7c100a15931c736
airbusramo <omarrefaat15@live.com>
2019-11-06 05:39:58 +0200

    Add project files.

---
+++ Mochan-Quick-Shape-Check-0/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Mochan_Quick_Shape_Check_0
+{
+    class Program
+    {
+        static void Main()
+        {
+            //This is not meant to be proper. I just gotta make this real fast and use it once or twice.
+            Console.WriteLine("Triangles clockwise. Small squares reading style. Finish row 1 before 2.");
+            Console.WriteLine();
+
+            var path = @"D:\Projects\Mochan_Projects\College\Pattern_Hell_0\Repeat.txt";
+
+            string[] lines = File.ReadAllLines(path, Encoding.UTF8);
+
+            foreach (string line in lines)
+            {
+                Console.WriteLine(line);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("You have {0} elements in your text file. It should be 28.", lines.Length);
+            Console.WriteLine();
+
+            foreach (var number in lines.GroupBy(x => x))
+            {
+                Console.WriteLine(number.Key + " repeats " + (number.Count() - 1) + " times");
+            }
+            Console.ReadKey();
+        }
+    }
+}
